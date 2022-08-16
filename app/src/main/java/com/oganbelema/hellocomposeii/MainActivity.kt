c83e6d1b4886495a77b5b1ceaee6a9f071de635c
Oganbelema <oganbelema@gmail.com>
2022-08-16 20:08:42 +0100

    made circle clickable and added text to display amount

--- app/src/main/java/com/oganbelema/hellocomposeii/MainActivity.kt
+++ app/src/main/java/com/oganbelema/hellocomposeii/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.oganbelema.hellocomposeii
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Surface
@@ -15,8 +14,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.oganbelema.hellocomposeii.ui.theme.HelloComposeIITheme
 
 class MainActivity : ComponentActivity() {
@@ -37,6 +39,18 @@ private fun MyApp() {
         modifier = Modifier.fillMaxSize(),
         color = Color(0xFF546E7A)
     ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "€100", style = TextStyle(
+                color = Color.White,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold
+            ))
+            Spacer(modifier = Modifier.height(130.dp))
+            CreateCircle()
+        }
 
     }
 }
@@ -46,8 +60,10 @@ private fun MyApp() {
 private fun CreateCircle() {
     Card( modifier = Modifier
         .padding(4.dp)
-        .size(45.dp),
-    shape = CircleShape) {
+        .size(105.dp)
+        .clickable { },
+    shape = CircleShape,
+    elevation = 6.dp) {
         Box(contentAlignment = Alignment.Center) {
             Text(text = "Tap")
         }
