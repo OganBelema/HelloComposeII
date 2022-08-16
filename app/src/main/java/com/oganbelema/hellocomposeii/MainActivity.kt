313a97c0172b7b904b9dce4e5a17e1fd8c7858bb
Oganbelema <oganbelema@gmail.com>
2022-08-16 19:35:37 +0100

    added circle and centered text in it

--- app/src/main/java/com/oganbelema/hellocomposeii/MainActivity.kt
+++ app/src/main/java/com/oganbelema/hellocomposeii/MainActivity.kt
@@ -3,14 +3,20 @@ package com.oganbelema.hellocomposeii
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.oganbelema.hellocomposeii.ui.theme.HelloComposeIITheme
 
 class MainActivity : ComponentActivity() {
@@ -18,36 +24,41 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HelloComposeIITheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+private fun MyApp() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color(0xFF546E7A)
+    ) {
+
+    }
 }
 
+@Preview
 @Composable
-fun SetAge(age: Int = 12) {
-    Text(text = age.toString())
+private fun CreateCircle() {
+    Card( modifier = Modifier
+        .padding(4.dp)
+        .size(45.dp),
+    shape = CircleShape) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(text = "Tap")
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HelloComposeIITheme {
-        Column {
-            Greeting("Android")
-            SetAge()
-        }
-
+        MyApp()
     }
 }
